3093c34859eb3f47cc027e1bbe6996970548ecf3
wwan0045 <54574030+wwan0045@users.noreply.github.com>
2021-10-13 13:45:42 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,3 +3,8 @@ vegaEmbed("#bar_chart", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_2 = "Week10_Homework.vg.json";
+vegaEmbed("#line_chart", vg_2).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
