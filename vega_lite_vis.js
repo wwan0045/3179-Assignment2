d664e7f32c85372f52ecff9954816affb054d8b9
wwan0045 <54574030+wwan0045@users.noreply.github.com>
2021-10-24 11:40:55 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,19 @@
 var vg_1 = "Week9_Homework.vg.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_2 = "Week10_Homework.vg.json";
-vegaEmbed("#line_chart", vg_2).then(function(result) {
+vegaEmbed("#multiline", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
+var vg_3 = "scatplot.vg.json";
+vegaEmbed("#scatplot", vg_3).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "hbar.vg.json";
+vegaEmbed("#hbar", vg_4).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
